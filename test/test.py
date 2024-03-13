8cafd50821cb629d288937f325836c515e18d291
jstepha8 <jstepha8@jhu.edu>
2024-03-12 21:55:28 -0500

    Adding ImageDataGenerator to enable training on all data.

--- test/test.py
+++ test/test.py
@@ -6,48 +6,50 @@ from keras.models import Model
 import cv2
 import os
 import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator
 
-from tensorflow.keras.utils import image_dataset_from_directory as idfd
+# Loads all images found and trains given model
+def load_images_and_train_model(count, model):
+    with tf.device('/GPU:0'):
+        dfY = pd.read_csv("../data/train_labels.csv")[:count * 100] # train on count number of image chains (each chain has 100 images)
+        # Generate path for each image (necessary for image generator)
+        dfY["path"] = [f"{id}/{i:03d}.png" for id, i in zip(dfY["chain_id"], dfY["i"])]
+        dfY.drop(["chain_id", "i"], axis=1, inplace=True)
+        train_datagen = ImageDataGenerator()
+        
+        # Create generator to read images from directories
+        train_generator = train_datagen.flow_from_dataframe(dataframe=dfY,
+            directory="../data/images",
+            x_col="path",
+            y_col=["x", "y", "z", "qw", "qx", "qy", "qz"],
+            target_size=(640, 512), # Downscale
+            color_mode='grayscale',
+            class_mode="raw")
+        
+        model.fit(train_generator, epochs=2)
+
+    return model
 
 
-dfY = pd.read_csv("../data/train_labels.csv")
-data = []
-dataY = []
-
-def load_images(count):
-    j = 0
-    for dir in os.listdir("../data/images"):
-        if j == count:
-            break
-        for img in os.listdir(f"../data/images/{dir}"):
-            #print(img)
-            data.append([tf.cast(cv2.imread(f"../data/images/{dir}/{img}", 0), dtype=tf.float32)])
-            i = int(img[:3])
-            #print(dfY.loc[(dfY.chain_id == dir) & (dfY.i == i), ["x", "y", "z", "qw", "qx", "qy", "qz"]].values)
-            dataY.append(dfY.loc[(dfY.chain_id == dir) & (dfY.i == i), ["x", "y", "z", "qw", "qx", "qy", "qz"]].values.tolist()[0])
-        j += 1
-    #print(data)
-    return np.array(data).reshape(-1, 1280, 1024, 1), pd.DataFrame(dataY, columns=["x", "y", "z", "qw", "qx", "qy", "qz"])
-
-dfX, dfY = load_images(10)
 # x_train = dfX[:9]
 # y_train = dfY[:9]
 # x_test = dfX[-9:]
 # y_test = dfY[-9:]
-print(dfY)
-print(dfX.shape)
-dfY.fillna(value=0, inplace=True)
+# print(dfY)
+# print(dfX.shape)
+# dfY.fillna(value=0, inplace=True)
 # plt.imshow(dfX.tolist(), cmap="gray") # plot 1st image's 2nd feature map
 # plt.show()
 
 
-input = Input(shape=(1280,1024,1))
-conv = Conv2D(4, 4, activation='relu', padding="same", input_shape=[1280, 1024, 1])(input)
+input = Input(shape=(640,512,1))
+conv = Conv2D(4, 4, activation='relu', padding="same", input_shape=[640, 512, 1])(input)
 #BatchNormalization()(conv)
 pooling = MaxPooling2D(2)(conv)
 conv2 = Conv2D(3, 2, activation='relu', padding='same')(pooling)
 flat = Flatten()(conv2)
 hidden = Dense(16, activation="relu")(flat)
+# Multiple outputs: https://stackoverflow.com/questions/44036971/multiple-outputs-in-keras
 x = Dense(1)(hidden)
 y = Dense(1)(hidden)
 z = Dense(1)(hidden)
@@ -60,4 +62,4 @@ model = Model(inputs=input, outputs=[x,y,z,qw,qx,qy,qz])
 
 model.summary()
 model.compile(loss="categorical_crossentropy", optimizer=tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9))
-model.fit(dfX, [dfY.loc[:, "x"], dfY.loc[:, "y"], dfY.loc[:, "z"], dfY.loc[:, "qw"], dfY.loc[:, "qx"], dfY.loc[:, "qy"], dfY.loc[:, "qz"]], epochs=10)
+load_images_and_train_model(2, model)
